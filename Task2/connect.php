33d5413389d6fa3d4b514a99700786c7b33be2e2
salmayoussef1 <salmayoussef741@gmail.com>
2025-07-07 02:56:15 +0300

    Add files via upload

---
+++ Task2/connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+echo "Connected ";
+
+?>
